179efac50928cea7b0d1cab986a932bd0603ef5d
Marco Cervantez <marco.cervantez@pm.me>
2025-03-31 07:42:15 -0500

    Adding Pester Test for Measure-R2PO.

---
+++ azure/powershell/Measure-R2PO.Tests.ps1
@@ -0,0 +1,29 @@
+BeforeAll {
+    . ./Measure-R2PO.ps1
+}
+
+Describe "Measure-R2PO" {
+    
+    It "Returns a valid object with expected properties" {
+        $result = Measure-R2PO
+
+        $result | Should -Not -BeNullOrEmpty
+        $result | Should -BeOfType [PSCustomObject]
+        $result.PSObject.Properties.Name | Should -Contain "AzCmdletCallTimeMs"
+        $result.PSObject.Properties.Name | Should -Contain "SubscriptionCount"
+        $result.PSObject.Properties.Name | Should -Contain "Subscriptions"
+    }
+
+    It "Measures execution time greater than zero" {
+        $result = Measure-R2PO
+
+        $result.AzCmdletCallTimeMs | Should -BeGreaterThan 0
+    }
+
+    It "Returns a valid number of subscriptions" {
+        $result = Measure-R2PO
+
+        $result.SubscriptionCount | Should -BeGreaterThanOrEqualTo 0
+        $result.Subscriptions | Should -Not -BeNullOrEmpty
+    }
+}
