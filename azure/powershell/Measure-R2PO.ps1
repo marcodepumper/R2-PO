4e7d35fc3dc373135c3ef03f1064328b712975fd
Marco Cervantez <marco.cervantez@pm.me>
2025-04-02 00:51:19 -0500

    Updating results to match Go test.

--- azure/powershell/Measure-R2PO.ps1
+++ azure/powershell/Measure-R2PO.ps1
@@ -1,42 +1,19 @@
 function Measure-R2PO {
-    <#
-    .SYNOPSIS
-        Measures latency and retrieves Azure subscriptions.
-
-    .DESCRIPTION
-        This function benchmarks the time taken to fetch the list of 
-        available subscriptions.
-
-    .NOTES
-        - Ensure authentication via `Connect-AzAccount` before running.
-
-    .OUTPUTS
-        PSCustomObject containing subscription details and API call timing.
-
-    .EXAMPLE
-        Measure-R2PO
-        Retrieves all subscriptions while measuring latency.
-    #>
-
     [CmdletBinding()]
     param()
 
     try {
-        # Start timing PowerShell Az Cmdlet
         $azCmdletStartTime = Get-Date
-
-        # Get Azure Subscription(s)
         $subscriptions = Get-AzSubscription -ErrorAction Stop
-
-        # Stop timing PowerShell Az Cmdlet
         $azCmdletEndTime = Get-Date
-        $azCmdletTime = $azCmdletEndTime - $azCmdletStartTime
+        $azCmdletTime = ($azCmdletEndTime - $azCmdletStartTime).TotalMilliseconds
 
-        # Output results
-        return [PsCustomObject]@{
-            AzCmdletCallTimeMs = $azCmdletTime.TotalMilliseconds
-            SubscriptionCount = $subscriptions.Count
+        $result = @{
+            SubscriptionsCallTimeMs = $azCmdletTime
+            SubscriptionCount       = $subscriptions.Count
         }
+
+        return $result | ConvertTo-Json -Depth 2
     }
     catch {
         Write-Error "Error retrieving Azure subscriptions: $_"
